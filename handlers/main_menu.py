adb417ba4f879f03659be28c383f9cf6d9d94b75
bavanDA <56412698+bavanDA@users.noreply.github.com>
2025-07-19 23:56:08 -0400

    feat: add main menu

---
+++ handlers/main_menu.py
@@ -0,0 +1,5 @@
+from keyboards.reply import get_main_menu
+from constants.texts import TEXTS
+
+async def handle_start(client, message):
+    await message.reply(TEXTS["welcome"], reply_markup=get_main_menu())
