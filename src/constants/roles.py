ee1240a206073aba504420fdb28cf4a117b56c25
bavanDA <56412698+bavanDA@users.noreply.github.com>
2025-07-31 13:58:17 -0400

    refactor(main_menu): use Roles enum for role handling

--- src/constants/roles.py
+++ src/constants/roles.py
@@ -1,6 +1,11 @@
 from enum import Enum
 
+
 class Roles(str, Enum):
     ADMIN = "admin"
     BUYER = "buyer"
     RECEIVER = "receiver"
+
+    @classmethod
+    def values(cls):
+        return [role.value for role in cls]
--- src/handlers/main_menu.py
+++ src/handlers/main_menu.py
@@ -1,5 +1,11 @@
 from keyboards.reply import get_main_menu
 from constants.texts import TEXTS
+from constants.roles import Roles
 
-async def handle_start(client, message,role):
+
+async def handle_start(client, message, role):
+    if (role == Roles.RECEIVER):
+        await message.reply(TEXTS["welcome_receiver"], reply_markup=get_main_menu(role))
+        return
+
     await message.reply(TEXTS["welcome"], reply_markup=get_main_menu(role))
