6e862dd0d3de54de37b28a673eeaae5d1db43e3c
bavanDA <56412698+bavanDA@users.noreply.github.com>
2025-07-31 13:47:00 -0400

    feat(orders_crud): add prefetching for order retrieval

--- src/database/orders_crud.py
+++ src/database/orders_crud.py
@@ -1,5 +1,5 @@
 from .models import Order
-
+from tortoise.expressions import F
 
 async def add_order(user_id, min_stars, max_stars, min_supply, max_supply, count, receiver_id):
     return await Order.create(
@@ -13,9 +13,17 @@ async def add_order(user_id, min_stars, max_stars, min_supply, max_supply, count
     )
 
 
+async def get_all_orders():
+    return await Order.filter().all().prefetch_related("user", "receiver")
+
+
 async def get_orders(user_id):
-    return await Order.filter(user_id=user_id).all()
+    return await Order.filter(user_id=user_id).all().prefetch_related("user", "receiver")
 
 
 async def remove_order(user_id, order_id):
     await Order.filter(id=order_id, user_id=user_id).delete()
+
+
+async def increment_completed_count(order_id: int):
+    await Order.filter(id=order_id).update(completed_count=F("completed_count") + 1)
