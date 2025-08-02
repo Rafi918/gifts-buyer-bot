772a9eb96066bea6d86d458a201b1f03e4fcd606
bavanDA <56412698+bavanDA@users.noreply.github.com>
2025-08-01 23:44:12 -0400

    feat(orders_crud): add get_orders_count function

--- src/database/orders_crud.py
+++ src/database/orders_crud.py
@@ -1,6 +1,7 @@
 from .models import Order
 from tortoise.expressions import F
 
+
 async def add_order(user_id, min_stars, max_stars, min_supply, max_supply, count, receiver_id):
     return await Order.create(
         user_id=user_id,
@@ -21,9 +22,14 @@ async def get_orders(user_id):
     return await Order.filter(user_id=user_id).all().prefetch_related("user", "receiver")
 
 
+async def get_orders_count(user_id):
+    return await Order.filter(user_id=user_id).count()
+
+
 async def remove_order(user_id, order_id):
     await Order.filter(id=order_id, user_id=user_id).delete()
 
 
 async def increment_completed_count(order_id: int):
     await Order.filter(id=order_id).update(completed_count=F("completed_count") + 1)
+    return await Order.get(id=order_id).prefetch_related("user", "receiver")
